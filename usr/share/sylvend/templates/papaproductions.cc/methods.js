9003ca19706332b25409223171323b0b313a5855
papaproductions <89939280+papaproductions@users.noreply.github.com>
2024-07-28 00:28:37 -0600

    complete website redesign, gabe added to thanks.html

--- usr/share/sylvend/templates/papaproductions.cc/methods.js
+++ usr/share/sylvend/templates/papaproductions.cc/methods.js
@@ -1,6 +1,7 @@
 function methods(options) {
+    let timeLockIndex = -1;
     const defaultMethods = require("../../default");
-    const { package, req, peopleManager, configManager } = options;
+    const { package, req, peopleManager, configManager, resetSearch } = options;
 
     function thanks({ params, replace, page }) {
         console.log(peopleManager.ready);
@@ -23,15 +24,38 @@ ${params.map(t => {
     return {
         ...(defaultMethods(options)),
         footer: ({ replace, page, params }) => {
-            return replace(page, `<hr>
-<div class="sylv-footer">
+            return replace(page, `<div class="sylv-footer">
     ${params[1] ? "" : "<a href=\"/\">Go back to main page</a>"}<span>${params[1] ? "" : ". "}${params[0]}</span><img class="sylv" src="/stuff/sylv-small.png" title="nyah!" alt="Sylveon">
 </div>`);
         },
         quoteThanks: ({ replace, page }) => {
             return thanks({ params: configManager.config.quotes.map(q => { return { user: q.author } }), replace, page })
         },
-        thanks
+        thanks,
+        timeLockStart: ({ start, replace, page, params }) => {  
+            timeLockIndex = start;
+            return replace(page, "");
+        },
+        timeLockEnd: ({ params, start, length, page }) => {
+            let remaining = params.timestamp - Date.now();
+            if(remaining <= 0) {
+                return replace(page, "");
+            }
+            let seconds = Math.floor(remaining / 1000);
+            let minutes = Math.floor(seconds / 60);
+            let hours = Math.floor(minutes / 60);
+            let days = Math.floor(hours / 24);
+            resetSearch();
+            return page.slice(0, timeLockIndex) + `${params.message}
+<p><b>Time remaining:</b></p>
+<h1><code class="timelock" id="${params.timestamp}">${zero(days)}:${zero(hours % 24)}:${zero(minutes % 60)}:${zero(seconds % 60)}</code></h1>` + page.slice(start + length);
+        }
     };
 }
+
+//how many times have i implemented this method across all of my projects? this thing is like, part of my muscular memory at this point.
+function zero(n, c = 2) {
+    return "0".repeat(c - n.toString().length) + n.toString();
+}
+
 module.exports = methods;
